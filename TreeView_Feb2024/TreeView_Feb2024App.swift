38de4553cf1103c3cd9fdfe66e1349585c97d38b
prashanthPadmasahli <prashanthkumarshettigar@gmail.com>
2024-02-07 21:37:43 +0530

    Initial Commit

---
+++ TreeView_Feb2024/TreeView_Feb2024App.swift
@@ -0,0 +1,17 @@
+//
+//  TreeView_Feb2024App.swift
+//  TreeView_Feb2024
+//
+//  Created by mac on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TreeView_Feb2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
